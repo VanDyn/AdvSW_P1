79d169aba791d194208a1fbc405169a69aab84cf
Calum Thompson <thompsoncalum@gmail.com>
2019-02-06 22:02:21 +0000

    update menu

--- Code/Part_1/src/Menu.java
+++ Code/Part_1/src/Menu.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -14,9 +15,9 @@ import java.util.TreeSet;
  */
 public class Menu {
 	
-	// Would TreeSet be a better choice?
+	// Would TreeMap be a better choice?
 	// Allow sorting by item ID code
-	private TreeSet<Object> menu;
+	private HashSet<Object> menu;
 	
 	/**
 	 * Constructor reads in the menu .txt file and stores it in the [Selected data structure]
@@ -30,18 +31,22 @@ public class Menu {
 			
 		BufferedReader buff = new BufferedReader(new FileReader("exampleMenu.txt"));
 		Scanner scan = new Scanner(buff);
-		
+		String[] data = new String[2];
 		while(scan.hasNextLine()) {
 			
 			String line = scan.nextLine();
-			String[] data = line.split(";");
+			data = line.split(";");
+			
+			String ID = data[0];
+			String description = data[1];
+			String cost = data[2];
 			
 			// create new item object using data
-			// add the object to the TreeSet
+			// add the object to the [data structure]
 		}
 		scan.close();
-		}catch(Exception e){
-			
+		}catch(FileNotFoundException e){
+			System.out.println("File not found");
 		}
 	}
 	
@@ -106,8 +111,16 @@ public class Menu {
 	 * @param			
 	 * @return
 	 */
-	public ArrayList<String> getCategoryMembers(String catID){
-		// TODO
+	public ArrayList<String> getCategoryMembers(String category){
+		
+		ArrayList<String> itemDeets = new ArrayList<String>();
+		String catID;
+		
+		switch(category){
+			case "Coffee" : 
+				catID = "1";
+		}
+		
 		return null;
 	}
 	
@@ -119,6 +132,9 @@ public class Menu {
 	 */
 	public ArrayList<String> itemDetails(String name){
 		// TODO
+		ArrayList<String> itemDeets = new ArrayList<String>();
+		
+		
 		return null;
 	}
 	
