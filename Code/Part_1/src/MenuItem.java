dba445d5bd5f704847a86cf65bc468080f9cb1ae
Gargazon <gargazon89@gmail.com>
2019-02-14 14:19:48 +0000

    Added category variable

--- Code/Part_1/src/MenuItem.java
+++ Code/Part_1/src/MenuItem.java
@@ -7,10 +7,12 @@ Contains id String, float cost and description String.
 public class MenuItem {
 
   private String id;
+  private String category;
   private float cost;
   private String description;
 
-  MenuItem(String id, float cost, String description){
+  // 
+  MenuItem(String id, String category, float cost, String description){
     this.id = id;
     this.cost = cost;
     this.description = description;
